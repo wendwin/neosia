8e79e227ec38c7947c858b083c7622a6568f7f04
wendwin <wendwin12@gmail.com>
2025-08-22 13:46:09 +0700

    memperbaiki question section

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -1,19 +1,23 @@
-<div class="w-full bg-primary text-white py-16 md:py-36">
-  <div class="max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center gap-10 md:gap-16 bg-transparent md:bg-sec-grey rounded-2xl px-6 py-10 md:px-12">
+<div class="w-full bg-primary text-white py-16 md:py-36 relative overflow-hidden">
+    <div
+        class="max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center gap-10 md:gap-16 bg-transparent md:bg-sec-grey rounded-2xl px-6 py-10 md:px-14 lg:p-20 relative lg:mt-16">
 
-    <div class="w-full md:w-1/2 order-1 md:order-2 flex justify-center md:justify-end">
-      <img src="{{ asset('img/qa.png') }}" alt="Question and Answer" class="w-full max-w-xs md:max-w-sm">
+        <div class="w-full order-1 md:order-2 flex justify-center md:justify-end">
+            <img src="{{ asset('img/qa.png') }}" draggable="false" alt="Question and Answer" class="w-full md:w-1/2 md:absolute md:bottom-0 right-10 z-10">
+        </div>
+
+        <div class="w-full md:w-xl order-2 md:order-1 text-center md:text-left space-y-6 max-w-lg relative overflow-hidden">
+            <h2 class="text-3xl md:text-4xl md:max-w-sm font-medium">
+                Still unsure and have some questions?
+            </h2>
+            <a href="#"
+                class="inline-flex items-center gap-2 bg-sec-yellow transition hover:bg-[#eaa615] text-black font-medium px-5 py-3 rounded-md w-fit mx-auto md:mx-0">
+                <ion-icon name="logo-whatsapp"></ion-icon>
+                <h6>Talk To Us</h6>
+            </a>
+        </div>
+        
+        <img class="hidden md:block md:absolute w-[50%] right-32" draggable="false" src="{{ asset('img/elipse question.png') }}" alt="shape">
     </div>
-
-    <div class="w-full md:w-1/2 order-2 md:order-1 text-center md:text-left space-y-6 max-w-lg">
-      <h2 class="text-2xl lg:text-4xl font-medium">
-        Still unsure and have some questions?
-      </h2>
-      <a href="#" class="inline-flex items-center gap-2 bg-yellow-400 hover:bg-yellow-300 text-black font-medium px-5 py-3 rounded-md w-fit mx-auto md:mx-0 transition">
-        <ion-icon name="logo-whatsapp"></ion-icon>
-        Talk To Us
-      </a>
-    </div>
-
-  </div>
+        
 </div>
